ed4b184b9527b4f1fe983bc0643fca6e1f8d123e
mk812y <mk812y@gmail.com>
2022-11-07 20:45:24 +0300

    Initial Commit

---
+++ SwiftUITutorial2/SwiftUITutorial2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial2App.swift
+//  SwiftUITutorial2
+//
+//  Created by Михаил Куприянов on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
