50b9010acdc17470cca362ebb500985d0acfbd72
mk812y <mk812y@gmail.com>
2022-11-07 21:21:52 +0300

    chapter one done

---
+++ SwiftUITutorial2/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorial2
+//
+//  Created by Михаил Куприянов on 07.11.2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
